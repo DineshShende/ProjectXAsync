f40f18ca2726810b7948e8ec9b79fd07247c8332
dinesh <dineshshe@gmail.com>
2015-01-01 18:39:50 +0530

    code cleaned

--- src/main/java/com/projectx/async/controller/SendVerificationDetailsController.java
+++ src/main/java/com/projectx/async/controller/SendVerificationDetailsController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.projectx.async.domain.EmailDTO;
 import com.projectx.async.domain.EventDeferredObject;
-import com.projectx.async.domain.RequestStore;
+
 
 
 
